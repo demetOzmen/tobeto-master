e2ce2d455ab13319e6ced1ca10958e91438e3985
Demet Özmen <147596982+demetOzmen@users.noreply.github.com>
2024-02-12 16:05:56 +0300

    edu page completed

--- src/pages/MyProfile/Education/Education.tsx
+++ src/pages/MyProfile/Education/Education.tsx
@@ -1,34 +1,129 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  Alert,
+  Card,
+  ListGroup,
+} from "react-bootstrap";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import Menu from "../Menu/Menu";
 import "./Education.css";
 
+type Education = {
+  educationLevel: string;
+  department: string;
+  graduationYear: Date | null;
+  startYear: Date | null;
+  university: string;
+  isContinuing: boolean;
+};
+
 export default function Education() {
-  const [education, setEducation] = useState({
+  const handleDeleteEducation = (index: number) => {
+    // Filter metoduyla seçilen index haricindeki tüm eğitimleri yeni bir dizi olarak geri dön
+    const updatedEducations = educations.filter((_, i) => i !== index);
+    setEducations(updatedEducations);
+  };
+
+  const [educations, setEducations] = useState<Education[]>([]);
+  const [error, setError] = useState<string>("");
+  const [currentEducation, setCurrentEducation] = useState<Education>({
     educationLevel: "",
     department: "",
-    graduationYear: "",
+    graduationYear: null,
+    startYear: null,
     university: "",
-    startYear: "",
     isContinuing: false,
   });
 
-  const handleSubmit = (event: React.FormEvent<any>) => {
-    event.preventDefault();
-    // Form submit logic here
-    console.log(education);
+  const handleDateChange = (
+    selectedDate: Date | null,
+    name: "startYear" | "graduationYear"
+  ) => {
+    setCurrentEducation((prev) => ({
+      ...prev,
+      [name]: selectedDate,
+    }));
+
+    // Tarih değerlerini kontrol et
+    const startYearDate =
+      name === "startYear" ? selectedDate : currentEducation.startYear;
+    const graduationYearDate =
+      name === "graduationYear"
+        ? selectedDate
+        : currentEducation.graduationYear;
+
+    if (
+      startYearDate &&
+      graduationYearDate &&
+      startYearDate > graduationYearDate
+    ) {
+      // Hata mesajını ayarla
+      setError("Başlangıç yılı mezuniyet yılından sonra olamaz.");
+    } else {
+      // Hata mesajını temizle
+      setError("");
+    }
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | any>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value, type, checked } = event.target;
-    setEducation((prev) => ({
+    setCurrentEducation((prev) => ({
       ...prev,
       [name]: type === "checkbox" ? checked : value,
     }));
   };
 
+  const handleAddEducation = () => {
+    if (
+      currentEducation.startYear &&
+      currentEducation.graduationYear &&
+      currentEducation.startYear <= currentEducation.graduationYear
+    ) {
+      setEducations([...educations, currentEducation]);
+      setCurrentEducation({
+        educationLevel: "",
+        department: "",
+        graduationYear: null,
+        startYear: null,
+        university: "",
+        isContinuing: false,
+      });
+      setError("");
+    } else {
+      setError("Başlangıç yılı mezuniyet yılından sonra olamaz.");
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleAddEducation();
+  };
+
+  const formatEducationLevel = (level: any) => {
+    switch (level) {
+      case "lisans":
+        return "Lisans";
+      case "on_lisans":
+        return "Ön Lisans";
+      case "yuksek_lisans":
+        return "Yüksek Lisans";
+      case "doktora":
+        return "Doktora";
+      default:
+        return "---";
+    }
+  };
+
   return (
-    <Container className="bordered">
+    <Container className="edu-cont bordered">
       <Row>
         <Col className="bordered" md={3} sm={12}>
           <Menu />
@@ -43,7 +138,7 @@ export default function Education() {
                     className="edc-input edc-select"
                     as="select"
                     name="educationLevel"
-                    value={education.educationLevel}
+                    value={currentEducation.educationLevel}
                     onChange={handleChange}
                   >
                     <option value="">Seviye Seçiniz</option>
@@ -61,7 +156,7 @@ export default function Education() {
                     className="edc-input"
                     type="text"
                     name="university"
-                    value={education.university}
+                    value={currentEducation.university}
                     onChange={handleChange}
                   />
                 </Form.Group>
@@ -75,7 +170,7 @@ export default function Education() {
                     className="edc-input"
                     type="text"
                     name="department"
-                    value={education.department}
+                    value={currentEducation.department}
                     onChange={handleChange}
                   />
                 </Form.Group>
@@ -83,12 +178,15 @@ export default function Education() {
               <Col md={6} sm={12}>
                 <Form.Group controlId="startYear">
                   <Form.Label className="edc-label">Başlangıç Yılı*</Form.Label>
-                  <Form.Control
-                    className="edc-input"
-                    type="text"
-                    name="startYear"
-                    value={education.startYear}
-                    onChange={handleChange}
+                  <DatePicker
+                    locale="tr"
+                    selected={currentEducation.startYear}
+                    onChange={(date) => handleDateChange(date, "startYear")}
+                    dateFormat="yyyy"
+                    showYearPicker
+                    placeholderText="Başlangıç Yılınızı Seçiniz"
+                    className="edc-input edc-input-date"
+                    isClearable
                   />
                 </Form.Group>
               </Col>
@@ -97,32 +195,88 @@ export default function Education() {
               <Col md={6} sm={12}>
                 <Form.Group controlId="graduationYear">
                   <Form.Label className="edc-label">Mezuniyet Yılı*</Form.Label>
-                  <Form.Control
-                    className="edc-input"
-                    as="select"
-                    name="graduationYear"
-                    value={education.graduationYear}
-                    onChange={handleChange}
-                  >
-                    <option value="">Mezuniyet Yılınızı Seçiniz</option>
-                  </Form.Control>
+                  <DatePicker
+                    locale="tr"
+                    selected={currentEducation.graduationYear}
+                    onChange={(date) =>
+                      handleDateChange(date, "graduationYear")
+                    }
+                    dateFormat="yyyy"
+                    showYearPicker
+                    placeholderText="Mezuniyet Yılınızı Seçiniz"
+                    className="edc-input edc-input-date"
+                    isClearable
+                  />
                 </Form.Group>
-                <Form.Group controlId="isContinuing" className="edc-check bordered">
+                <Form.Group controlId="isContinuing" className="edc-check ">
                   <Form.Check
                     className="edc-check-box"
                     type="checkbox"
                     name="isContinuing"
                     label="Devam Ediyorum"
-                    checked={education.isContinuing}
+                    checked={currentEducation.isContinuing}
                     onChange={handleChange}
                   />
                 </Form.Group>
               </Col>
             </Row>
+
             <Button className="button-edc" type="submit">
               Kaydet
             </Button>
+            {error && <Alert variant="danger">{error}</Alert>}
           </Form>
+          <Row>
+            {/* Eğitim bilgilerinin listelendiği bölüm */}
+
+            <div className="education-list">
+              {educations.map((edu, index) => (
+                <div key={index} className="education-card ">
+                  <div className="education-header">
+                    <div className="education-year">
+                      <img src="https://tobeto.com/grade-date.svg"></img>&nbsp;
+                      {edu.startYear ? edu.startYear.getFullYear() : "???"} -
+                      {edu.graduationYear
+                        ? edu.graduationYear.getFullYear()
+                        : "???"}
+                    </div>
+                    <div className="education-level-badge">
+                      {formatEducationLevel(edu.educationLevel)}
+                    </div>
+                  </div>
+                  <Row>
+                    <Col md={5} xs={12} className="education-university ed-head">
+                      Üniversite
+                    </Col>
+                    <Col md={7} sm={12} className="education-department ed-head ed-dep-head">
+                      Bölüm
+                    </Col>
+                  </Row>
+                  <Row >
+                    <Col md={5} sm={12}>
+                      <div className="education-university ed-sub">
+                        {edu.university}
+                      </div>
+                    </Col>
+                    <Col md={6} sm={12}>
+                      <div className="education-department ed-sub">
+                        {edu.department}
+                      </div>
+                    </Col>
+
+                    <Col md={1} sm={12}>
+                      <button
+                        onClick={() => handleDeleteEducation(index)}
+                        className="delete-button-edu"
+                      >
+                        <FontAwesomeIcon icon={faTrashAlt} />
+                      </button>{" "}
+                    </Col>
+                  </Row>
+                </div>
+              ))}
+            </div>
+          </Row>
         </Col>
       </Row>
     </Container>
